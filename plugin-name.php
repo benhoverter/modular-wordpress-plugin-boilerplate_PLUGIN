7f7c6271e86259faf8923d901c2285785c3238d3
Ben Hoverter <32376575+benhoverter@users.noreply.github.com>
2019-04-19 13:58:54 -0700

    1.0.10 test version

--- plugin-name.php
+++ plugin-name.php
@@ -9,14 +9,14 @@
 * that starts the plugin.
 *
 * @link              http://github.com/benhoverter/modular-wordpress-plugin-boilerplate
-* @since             1.0.7
+* @since             1.0.10
 * @package           plugin-name
 *
 * @wordpress-plugin
 * Plugin Name:       Plugin Name
 * Plugin URI:        https://github.com/benhoverter/modular-wordpress-plugin-boilerplate
 * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
-* Version:           1.0.7
+* Version:           1.0.10
 * Author:            Your Name
 * Author URI:        http://example.com
 * License:           GPL-2.0+
@@ -37,7 +37,7 @@ if ( !defined( 'WPINC' ) ) {
 * Start at version 1.0.0 and use SemVer - https://semver.org
 * Rename this for your plugin and update it as you release new versions.
 */
-define( 'PLUGIN_NAME_VERSION', '1.0.7' );
+define( 'PLUGIN_NAME_VERSION', '1.0.10' );
 
 /**
 * The code that runs during plugin activation.
